77ca01606cfbb9d2c34585519650eaa18791e886
ZS2005-prog <zacharystair@gmail.com>
2025-11-17 21:46:25 -0500

    I have completed all the steps. Please let me know if their are any errors or if I missed anything.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chapter 11 project</title>
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+    <h1>Building a Database with Stored Procedures</h1>
+    <p>Zachary Stair</p>
+    <p>November 17th 2025</p>
+</body>
+</html>
